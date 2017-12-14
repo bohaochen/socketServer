1409b9b09a757bf5ed009eb35748ccfe90cae5b3
bohaoChen <360485406@qq.com>
2017-12-14 22:20:30 +0800

    one

--- server.js
+++ server.js
@@ -89,9 +89,9 @@ io.on('connection', function (socket) {
     
 });
 
-server.listen(3010, '0.0.0.0', function (err) {
+server.listen(8080, '0.0.0.0', function (err) {
     if (err) {
         return console.log(err);
     }
-    console.log('Listening at http://0.0.0.0:3010');
+    console.log('Listening at http://0.0.0.0:8080');
 });
